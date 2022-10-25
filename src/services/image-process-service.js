d3e6aa4b1da7632beb4e04f0c271d76c2975293d
kanchana weerasekera <kanchanawee@gmail.com>
2022-10-25 07:29:21 +0530

    Add axios and modemon

--- src/services/image-process-service.js
+++ src/services/image-process-service.js
@@ -1,21 +1,21 @@
-let request = require("request");
+let axios = require("axios");
 let argv = require("minimist")(process.argv.slice(2));
 let blend = require("@mapbox/blend");
 let { writeFile } = require("fs");
 let { join } = require("path");
 
 const generateImage = async (firstReq) => {
-  request.get(firstReq, (err, res, body) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Received response with status:" + res.statusCode);
-    return body;
-  });
+  try {
+    const res = await axios.get(firstReq.url, { encoding: "binary" });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const blendImage = async (firstBody, secondBody) => {
+  let width = 400;
+  let height = 500;
   blend(
     [
       { buffer: new Buffer(firstBody, "binary"), x: 0, y: 0 },
@@ -23,16 +23,20 @@ const blendImage = async (firstBody, secondBody) => {
     ],
     { width: width * 2, height: height, format: "jpeg" },
     (err, data) => {
-      const fileOut = join(process.cwd(), `/cat-card.jpg`);
+      try {
+        const fileOut = join(process.cwd(), `/cat-card.jpg`);
 
-      writeFile(fileOut, data, "binary", (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
+        writeFile(fileOut, data, "binary", (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
 
-        console.log("The file was saved!");
-      });
+          console.log("The file was saved!");
+        });
+      } catch (error) {
+        console.log("blend error", error);
+      }
     }
   );
 };
@@ -61,7 +65,6 @@ const getImage = async () => {
         color +
         "&s=" +
         size,
-      encoding: "binary",
     };
 
     let secondReq = {
@@ -76,7 +79,6 @@ const getImage = async () => {
         color +
         "&s=" +
         size,
-      encoding: "binary",
     };
 
     const [firstBody, secondBody] = await Promise.all([
